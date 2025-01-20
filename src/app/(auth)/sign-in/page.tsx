9eeb3d93153cc58ab085c9e20b60ac1b6ff24f5d
Sonikm <sonikm443@gmail.com>
2025-01-21 01:23:54 +0530

    created sign up form

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,26 +1,7 @@
-"use client";
-import { useSession, signIn, signOut } from "next-auth/react";
-
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <>
-        <p>Signed in as {session.user.email}</p> <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  }
+const SignInPage = () => {
   return (
-    <>
-      <p>Not signed in</p>
-      <br />
-      <button
-        className="bg-orange-500 p-2 rounded m-4"
-        onClick={() => signIn()}
-      >
-        Sign in
-      </button>
-    </>
-  );
+    <div>SignInPage</div>
+  )
 }
+
+export default SignInPage
